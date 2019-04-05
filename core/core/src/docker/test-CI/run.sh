546f6e269e79c83d98e304e56591b64fa682d165
Przemek <przemyslaw.dyrda@pl.abb.com>
2019-04-05 16:55:07 +0200

    Added Dockerfile for test containers

--- core/core/src/docker/test-CI/run.sh
+++ core/core/src/docker/test-CI/run.sh
@@ -1,6 +1,8 @@
 cd  /epiphany/
 git init -q
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
+echo "ls"
+ls /epiphany/core/data/azure/infrastructure
 echo "Preparing credentials"
 
 bash core/core/src/docker/test-CI/prepare_sp.sh
